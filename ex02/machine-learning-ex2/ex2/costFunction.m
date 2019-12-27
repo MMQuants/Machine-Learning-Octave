74054d11d580d0cbb1a80f9235a5199b1b192368
MMQuants <mar4o86@mail.bg>
2019-12-27 18:28:10 +0200

    cost function added

--- ex02/machine-learning-ex2/ex2/costFunction.m
+++ ex02/machine-learning-ex2/ex2/costFunction.m
@@ -21,6 +21,14 @@ grad = zeros(size(theta));
 %
 
 
+h = sigmoid(X*theta);
+
+% J = (1/m)*sum(-y.*log(h)-(1-y).*log(y-h));
+J= (1/m)*(-y'*log(h)-(1-y)'*log(1-h));
+
+grad = (1/m)*X'*(h-y);
+
+
 
 
 
